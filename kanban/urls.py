2e2fe5d712cdfb6db37d67423de73210c59ecace
uch1104 <foajoaquin@gmail.com>
2021-02-08 21:25:26 +0900

    ユーザー編集機能の実装

--- kanban/urls.py
+++ kanban/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("home/", views.home, name="home"),
     path('signup/', views.signup, name='signup'),
     path("users/<int:pk>/", views.UserDetailView.as_view(), name="users_detail"),
+    path("users/<int:pk>/update/", views.UserUpdateView.as_view(), name="users_update"),
 ]
--- kanban/views.py
+++ kanban/views.py
@@ -2,8 +2,11 @@ from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
-from django.views.generic import DetailView
+from django.shortcuts import render, redirect, resolve_url
+from django.views.generic import DetailView, UpdateView
+
+from .forms import UserForm
+from .mixins import OnlyYouMixin
 
 # Create your views here.
 # from django.http import HttpResponse
@@ -35,3 +38,11 @@ def signup(request):
 class UserDetailView(DetailView):
     model = User
     template_name = "kanban/users/detail.html"
+
+class UserUpdateView(OnlyYouMixin, UpdateView):
+    model = User
+    template_name = "kanban/users/update.html"
+    form_class = UserForm
+
+    def get_success_url(self):
+        return resolve_url('kanban:users_detail', pk=self.kwargs['pk'])
