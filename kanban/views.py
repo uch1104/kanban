3a7fdbea0ed19ab211102e701fc6450c02ec5119
uch1104 <foajoaquin@gmail.com>
2021-02-09 13:34:45 +0900

    編集画面の修正

--- kanban/views.py
+++ kanban/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, resolve_url
 from django.views.generic import DetailView, UpdateView
@@ -35,7 +36,7 @@ def signup(request):
     }
     return render(request, 'kanban/signup.html', context)
 
-class UserDetailView(DetailView):
+class UserDetailView(LoginRequiredMixin, DetailView):
     model = User
     template_name = "kanban/users/detail.html"
 
